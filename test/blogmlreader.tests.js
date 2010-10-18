155e8ccad9ea4cb65082225b1c85a10f7baadce0
chris.nicola <chris.nicola@visioncritical.com>
2010-10-17 23:43:53 -0700

    Basic converstion from BlogML to XWR working

--- test/blogmlreader.tests.js
+++ test/blogmlreader.tests.js
@@ -4,16 +4,71 @@ var dsl = require('nodeunit-dsl'),
 		before = dsl.before, after = dsl.after,
 		sys = require('sys'),
 		fs = require('fs'),
-		bmlReader = require('../lib/blogmlreader.js');
-		
+		bmlReader = require('../lib/blogmlreader'),
+		wordpress = require('../lib/wordpress');	
+
 if (module.id === '.') { run(__filename); }
 
+var fixPostCats = function(catalog, post) {
+	if (post.categories) { 
+		var postCats = post.categories.category;
+		for (var k = 0; k < postCats.length; k++) {
+			postCats[k].attr.ref = catalog[postCats[k].attr.ref];
+		}
+	}
+};
+
+var addTags = function(tagList, post) {
+	if (post.tags) {
+		var tags = post.tags.tag;
+		for(var j=0; j < tags.length; j++) {
+			tagList[tags[j].attr.ref] = {};
+			tagList[tags[j].attr.ref].name = tags[j].attr.ref;
+		}
+	}
+};
+
+var fixPostComments = function (post) {
+	if (post.comments && post.comments.comment){
+		var idMap = {};
+		var comments = post.comments.comment;
+		for (var i = 0; i < comments.length; i++) {
+			idMap[comments[i].attr.id] = i + 1;
+			comments[i].attr.id = i + 1;
+			var parent = idMap[comments[i].attr.parentid]; 
+			comments[i].attr.parentid = parent ? parent : 0;
+		}
+	}
+};
+
+var fixBlogData = function(blog) {
+	var catalog = {};
+	var categories = blog.categories.category;
+	blog.tags = {};
+	for (var i=0; i < categories.length; i++) {
+		catalog[categories[i].attr.id] = categories[i].title.text;
+	}
+	var posts = blog.posts.post;
+	for (var j = 0; j < posts.length; j++) {
+		posts[j].attr.id = posts.length - j;
+		fixPostCats(catalog, posts[j]);
+		addTags(blog.tags, posts[j]);
+		fixPostComments(posts[j]);
+	}
+	return blog;
+};
+
+
+
 test('Can read from blogml file', function(t) {
-	t.expect(1);
 	fs.readFile(__dirname + '/data/BlogML.xml', function (err, data) {
 		bmlReader.read(data, function(jsObj){
-			t.equal(jsObj.title.text,'lucisferre');
-			t.done();
+			jsObj = fixBlogData(jsObj);
+			wordpress.writeBlog(jsObj, function(output){
+				console.log(output);
+				t.done();
+			});
 		});
 	});
 });
+
