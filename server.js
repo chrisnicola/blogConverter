6c1ae79df3a14e5cd21f166fcea5d79579f09b05
chris.nicola <chris.nicola@visioncritical.com>
2010-10-19 08:43:26 -0700

    Adding file upload page for deployment

--- server.js
+++ server.js
@@ -1,5 +1,52 @@
+require.paths.unshift(__dirname + '/vendor');
 var sys = require('sys'),
-    xml2js = require('xml2js');
+    formidable = require('formidable'),
+    http = require('http'),
+    nstatic = require('node-static/lib/node-static'),
+    url = require('url'),
+    utl = require('util'),
+    convertor = require('./lib/convertor');
 
-var parser = new xml2js.Parser();
+function upload_file(req, res) {
+    var form = new formidable.IncomingForm()
+        , files = []
+        , fields = [];
+    
+    form.uploadDir = '/tmp';
+    form.on('field', function(field, value){
+        //p([field, value]);
+        fields.push([field, value]);
+    });
+    form.on('file', function (field, file) {
+        //p([field, file]);
+        files.push([fields, file]);
+    });
+    form.on('end', function(){
+        sys.log('Done');
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('testing');
+        res.end(sys.inspect({fields: fields, files: files}));
+
+    });
+    form.parse(req); 
+    return;
+}
+
+var staticFiles = new nstatic.Server('./public', { cache: false });
+
+var server = http.createServer(function(req,res) {
+    var location = url.parse(req.url, true);
+    sys.log(sys.inspect(location));
+    switch (location.pathname) {
+        case '/upload':
+            upload_file(req, res);
+            break;
+        default:
+            staticFiles.serve(req, res);
+    }
+});
+
+server.listen(81);
+sys.log('Started...');
+
 
